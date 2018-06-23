d33d9635d9dfd093e794953e7e39e51d04891033
Saadettin Sivaz <sivaz05@gmail.com>
2018-06-24 01:16:23 +0300

    Add Question core type

--- src/com/seytar/survey/QuestionCore.java
+++ src/com/seytar/survey/QuestionCore.java
@@ -1,4 +1,23 @@
 package com.seytar.survey;
 
-public class QuestionCore {
+import java.util.ArrayList;
+
+public class QuestionCore extends QuestionAbstract {
+    @Override
+    public String getKey() {
+        return "core";
+    }
+
+    @Override
+    public ArrayList<Class> getAvailableAnswerTypes() {
+        ArrayList<Class> availableAnswerTypes = new ArrayList<Class>();
+        availableAnswerTypes.add(AnswerChoice.class);
+        availableAnswerTypes.add(AnswerOpenEnded.class);
+        return availableAnswerTypes;
+    }
+
+    @Override
+    public String getChoiceType() {
+        return ChoiceType.CHOICE_TYPE_MULTIPLE;
+    }
 }
